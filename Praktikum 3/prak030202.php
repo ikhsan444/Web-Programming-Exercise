5e7bacc4d0763063597005a55dc18440e9921146
ikhsan444 <71232773+ikhsan444@users.noreply.github.com>
2021-04-07 23:46:04 +0700

    PemrogramanWeb2021

--- Praktikum 3/prak030202.php
+++ Praktikum 3/prak030202.php
@@ -1,17 +1,17 @@
 <?php
-	function hitungDenda($d1,$d2){
-		$exp1=explode("-",$d1);
-		$dd1=$exp1[2];
-		$mm1=$exp1[1];
-		$yyyy1=$exp1[0];
+	function hitungDenda($tgl1,$tgl2){
+		$exp1=explode("-",$tgl1);
+		$date1=$exp1[2];
+		$month1=$exp1[1];
+		$year1=$exp1[0];
 
-		$exp2=explode("-",$d2);
-		$dd2=$exp2[2];
-		$mm2=$exp2[1];
-		$yyyy2=$exp1[0];
+		$exp2=explode("-",$tgl2);
+		$date2=$exp2[2];
+		$month2=$exp2[1];
+		$year2=$exp1[0];
 
-		$jd1=GregorianToJD($mm1, $dd1,$yyyy1 );
-		$jd2=GregorianToJD($mm2, $dd2,$yyyy2 );
+		$jd1=GregorianToJD($month1, $date1,$year1 );
+		$jd2=GregorianToJD($month2, $date2,$year2 );
 
 		$selisih=$jd2-$jd1;
 		$denda=$selisih*3000;
