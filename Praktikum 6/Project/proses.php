a15fc0df0c62611532cc533f23e83e976eb8b7f7
ikhsan444 <71232773+ikhsan444@users.noreply.github.com>
2021-04-29 15:49:44 +0700

    Praktikum 6 / Project / Login Cookie

---
+++ Praktikum 6/Project/proses.php
@@ -0,0 +1,31 @@
+<?php
+// daftar users
+$users = array(
+			array("username1", "Rosihan Ari Yuana", "123456"),
+			array("username2", "Dwi Amalia Fitriani", "654321"),
+			array("username3", "Faza Fauzan Khosyiyarrohman", "112233")
+		 );
+
+// jika form login sudah submitted
+if (isset($_POST['submit'])){
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	// proses pengecekan ada tidaknya username dan password 
+	// dalam daftar user
+	foreach ($users as $profile_user) {
+		if (($profile_user[0] == $_POST['username'])&&($profile_user[2] == $_POST['password'])){
+			// jika ada yg match maka bikin session untuk simpan nama user
+			setcookie('namauser', $profile_user[1]);
+
+			// redirect halaman ke page1.php
+			header("Location: page1.php");
+		}
+	}
+
+	// jika tidak ada username dan password yg match
+	echo "<h3>Login gagal</h3>";
+	echo "<p>Silakan <a href='form.html'>login kembali</a></p>";
+}
+
+?>
